f24411035a579510a158784a60562faaa2a52ae3
avdeevam <avdeev.am471@gmail.com>
2021-04-11 17:05:15 +0300

    feat: add solutions to task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,70 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = matrix.slice();
+  for (let i = 0; i < matrix.length; i++) {
+    res[i] = new Array(0);
+    for (let j = 0; j < matrix[i].length; j++) {
+      res[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (Array.isArray(matrix[i]) === true) {
+      for (let j = 0; j < matrix.length; j++) {
+        if (matrix[i][j] === true) {
+          // строки есть снизу
+          if (matrix.length - i > 1 && i === 0) {
+            res[i + 1][j]++;
+            if (matrix[i].length - j > 1) {
+              res[i][j + 1]++;
+              res[i + 1][j + 1]++;
+            }
+            if (j > 0) {
+              res[i][j - 1]++;
+              res[i + 1][j - 1]++;
+            }
+          }
+          // строки есть сверху и снизу
+          if (matrix.length - i > 1 && i > 0) {
+            res[i - 1][j]++;
+            res[i + 1][j]++;
+            if (matrix[i].length - j > 1) {
+              res[i][j + 1]++;
+              res[i - 1][j + 1]++;
+              res[i + 1][j + 1]++;
+            }
+            if (j > 0) {
+              res[i][j - 1]++;
+              res[i + 1][j - 1]++;
+              res[i - 1][j - 1]++;
+            }
+          }
+          // строки есть сверху
+          if (matrix.length - i === 1 && i > 0) {
+            res[i - 1][j]++;
+            if (matrix[i].length - j > 1) {
+              res[i][j + 1]++;
+              res[i - 1][j + 1]++;
+            }
+            if (j > 0) {
+              res[i][j - 1]++;
+              res[i - 1][j - 1]++;
+            }
+          }
+        }
+      }
+    } else if (matrix[i] === true) {
+      // только одна строка во всей матрице
+      if (matrix.length - i > 1) {
+        res[i + 1]++;
+      }
+      if (i > 0) {
+        res[i - 1]++;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = minesweeper;
